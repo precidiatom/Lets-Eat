97078f3b4afbd8de9e28de6c8b84c9c5a62b6ef5
Edmund Yao <eyao5@my.bcit.ca>
2018-11-08 11:20:37 -0800

    component folder

---
+++ component/galleryComp.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Text } from 'react-native';
+
+
+export default class asdfComponent extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+
+
+
+    componentDidMount() {
+    }
+
+  
+    render() {
+        return (
+
+        <View style={styles.containter}>
+        </View>
+
+        )
+    }
+}
+const styles = StyleSheet.create({
+    
+    containter: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+
+})
+
