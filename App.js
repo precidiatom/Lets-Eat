743bb650e6d2a76b94668e1cef6d2c757763e1e3
dvnii <dani.braceros@gmail.com>
2018-11-13 12:22:20 -0800

    navigation installed

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+
+import HomeScreen from './component/HomeScreen';
+import EatInScreen from './component/EatInScreen';
+import EatOutScreen from './component/EatOutScreen';
+
+const RootStack = createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    EatIn: {
+      screen: EatInScreen,
+    },
+    EatOut: {
+      screen: EatOutScreen,
+    },
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    return <RootStack/>;
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
